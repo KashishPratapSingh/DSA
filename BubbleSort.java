134aefc12a89b348c50752e35358a0da23f396c1
Kashish Pratap Singh <singhkashishpratap@gmail.com>
2024-10-08 20:54:46 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        bubbleSort(arr);
+    }
+    static void bubbleSort(int[] arr){
+        int n=arr.length;
+        for(int i=n-1;i>0;i--){
+            int didSwap=0;
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                    didSwap=1;
+                }
+            }
+            if(didSwap==0){
+                break;
+            }
+            System.out.println("Run");
+        }
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
